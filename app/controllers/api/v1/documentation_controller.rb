4eb6af1cc79e45ceae5dd58907716e9383a338b8
akabishau <aleksey.kabishau@icloud.com>
2024-12-01 23:21:26 -0500

    add update single documentation item route

--- app/controllers/api/v1/documentation_controller.rb
+++ app/controllers/api/v1/documentation_controller.rb
@@ -3,6 +3,8 @@ require "csv"
 module Api
   module V1
     class DocumentationController < ApplicationController
+      protect_from_forgery with: :null_session
+
       def index
         schema = Ctgov::V1Schema.all
         mappings = Ctgov::V1Mapping.all
@@ -15,6 +17,19 @@ module Api
           format.json { render json: docs }
           format.csv { send_data documentation_service.generate_csv(docs), filename: "aact_documentation.csv" }
         end
+      end
+
+      def update
+        record = Ctgov::V1Schema.find(params[:id])
+        if record.update(doc_params)
+          render json: record, status: :ok
+        else
+          render json: record.errors, status: :unprocessable_entity
+        end
+      end
+
+      def doc_params
+        params.require(:documentation).permit(:active, :description)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get "documentation", to: "documentation#index"
+      resources :documentation, only: [ :index, :update ]
     end
 
     namespace :v2 do
