75b0f18e9323bacd99a712b1f0033b0c87cf6373
akabishau <aleksey.kabishau@icloud.com>
2025-10-30 09:13:02 -0400

    Set Active Job queue adapters for all environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,7 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   # config.cache_store = :mem_cache_store
 
-  # Replace the default in-process and non-durable queuing backend for Active Job.
-  # config.active_job.queue_adapter = :resque
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -87,4 +86,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
+  # Changes from the default configuration:
+  config.active_job.queue_adapter = :sidekiq
 end
