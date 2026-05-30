8d6ea483a8af805de10cc5b1415f296bc5d27a1d
akabishau <aleksey.kabishau@icloud.com>
2026-05-30 17:07:45 -0400

    update test

--- test/components/stat_card_component_test.rb
+++ test/components/stat_card_component_test.rb
@@ -3,10 +3,23 @@
 require "test_helper"
 
 class StatCardComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(StatCardComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_the_label_and_value
+    render_inline(StatCardComponent.new(label: "Total AACT Users", value: "10,358"))
+
+    assert_text "Total AACT Users"
+    assert_text "10,358"
+  end
+
+  def test_value_is_styled_as_the_prominent_number
+    render_inline(StatCardComponent.new(label: "Total AACT Users", value: "10,358"))
+
+    # there is one visible div carrying both styles whose text includes 10,358
+    assert_selector "div.text-xl.font-bold", text: "10,358", count: 1
+  end
+
+  def test_accepts_non_string_values
+    render_inline(StatCardComponent.new(label: "Total Snapshots", value: 10_358))
+
+    assert_text "10358"
   end
 end
